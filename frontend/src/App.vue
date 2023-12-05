cfe1caef06e7a077b209c5009caf413c244d95e4
Petr (Laptop) <petrbohac3@seznam.cz>
2023-12-05 21:32:44 +0100

    reinstall a pridal sme vuetify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,52 +1,6 @@
-<script setup>
-
-
-import axios, {post} from "@/axios";
-
-function login() {
-  post('/auth/login', {
-    username: 'admin',
-    password: 'admin',
-  })
-}
-
-function logout() {
-  post('/auth/logout')
-}
-
-function hello() {
-  axios.get('/')
-      .then(res => {
-        console.log(res)
-      }).catch(err => {
-    console.log(err)
-  })
-}
-
-function secure() {
-  axios.get('/secure')
-      .then(res => {
-        console.log(res)
-      }).catch(err => {
-    console.log(err)
-  })
-}
-
-function state() {
-  axios.get('/auth/state')
-      .then(res => {
-        console.log(res)
-      }).catch(err => {
-    console.log(err)
-  })
-}
-
-</script>
-
 <template>
-  <button @click="login">login</button>
-  <button @click="logout">logout</button>
-  <button @click="hello">hello</button>
-  <button @click="secure">secure</button>
-  <button @click="state">state</button>
+
 </template>
+
+<script>
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,14 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import 'vuetify/styles'
+import {createApp} from 'vue'
+import {createVuetify} from 'vuetify'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from "@/router";
+
+const app = createApp(App);
+const vuetify = createVuetify();
+
+app.use(vuetify);
+app.use(router);
+
+app.mount('#app');

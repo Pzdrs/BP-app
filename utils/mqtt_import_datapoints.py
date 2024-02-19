4e5d138374acdf94fa622c4aebbe65406af1c293
Petr (Desktop) <petrbohac3@seznam.cz>
2024-02-19 17:08:51 +0100

    dalsi mockovaci tool

---
+++ utils/mqtt_import_datapoints.py
@@ -0,0 +1,32 @@
+import json
+import random
+import time
+
+import paho.mqtt.client as mqtt
+
+from credentials import *
+from kml_extract import get_coords
+
+FREQUENCY = 1000
+SOURCE = "B0:A7:32:29:D4:60"
+
+if __name__ == '__main__':
+
+    client = mqtt.Client(clean_session=True, callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+
+    client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+    print("Connecting...")
+    client.connect("10.0.0.20")
+    client.loop_start()
+
+    coords = get_coords('data/activa.kml')
+    for i, (lat, lng) in enumerate(coords):
+        client.publish('gnss', json.dumps({
+            'source': SOURCE,
+            'lat': lat, 'lng': lng, 'alt': 0,
+            'speed': random.randint(0, 150), 'course': random.randint(0, 360),
+        }))
+        print(f'Published {lat}, {lng}')
+        time.sleep(FREQUENCY / 1000)
+
+    client.disconnect()

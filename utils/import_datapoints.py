4bc495531c3d936a7b14a76b55d6431e8cd2f3ad
Petr (Desktop) <petrbohac3@seznam.cz>
2024-02-25 00:18:53 +0100

    more routes

--- utils/import_datapoints.py
+++ utils/import_datapoints.py
@@ -6,16 +6,19 @@ from bson import ObjectId
 
 from kml_extract import get_coords
 
+SOURCE = '65da5ea2f0d49e189faa9097'
+FILE = 'data/officeo.kml'
+
 if __name__ == '__main__':
     MONGO_CLIENT = pymongo.MongoClient("mongodb://admin:admin@localhost:27017/")
     DATABASE = MONGO_CLIENT["es-gps"]
 
-    coords = get_coords('data/activa.kml')
+    coords = get_coords(FILE)
     for i, (lat, lng) in enumerate(coords):
         DATABASE.datapoints.insert_one({
             'lat': lat, 'lng': lng, 'alt': 0,
-            'source': ObjectId('65c16c4b61bfc0617755e82a'),
-            'timestamp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=i),
+            'source': ObjectId(SOURCE),
+            'timestamp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=i,days=7),
             'course': random.randint(0, 360), 'speed': random.randint(0, 150)
         })
         print(f'Inserted {lat}, {lng}')
